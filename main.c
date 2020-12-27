faddc29fe08890e4732f5ce6df7a0ee7c65e7590
piotrwyrw <pw.piotr.wyrwas@gmail.com>
2020-12-27 11:42:21 +0100

    Constant variables & functions, Cleanup

--- main.c
+++ main.c
@@ -7,17 +7,25 @@
 #include <string.h>
 #include <math.h>
 
+#define MATH_C_VERSION 2.0	// Current version
+
+#define IS_LETTER(x) ((x  >= 'a' && x <= 'z') || x == '_')
+#define IS_DIGIT(x) (x >= '0' && x <= '9')
+
 float last_result = 0.0f;	// Holds the last result
+float fPass = 0.0f;			// This value gets passed to the functions
 char *input;				// Input to parse
+char *last_input; 			// Last input
 int pos = 0;				// Position of the lexer
 int should_output = 1;		// 1 if the float output should be printed (0 for variable/function declarations)
-float fPass = 0.0f;			// This value gets passed to the functions
+int should_exit = 0;		// 1 if the program should exit on the next iteration
 
 typedef struct VARIABLE VARIABLE;
 
 struct VARIABLE {
 	char *identifier;
 	float value;
+	int constant;
 };
 
 int variables_count = 0;
@@ -47,46 +55,51 @@ float get_variable(char *identifier) {
     return 0;
 }
 
-VARIABLE *create_variable(char *identifier, float value) {
+VARIABLE *create_variable(char *identifier, float value, int constant) {
 	VARIABLE *var = (VARIABLE*) malloc(sizeof(VARIABLE));
 	var->identifier = identifier;
 	var->value = value;
+	var->constant = constant;
 	return var;
 }
 
-void add_variable(char *id, float val) {
-    variables[++ variables_count] = create_variable(id, val); 
-}
-
-void mod_variable(char *id, float nVal) {
-	if (variable_exists(id)) {
-		for (int i = 0; i < sizeof(variables) / sizeof(VARIABLE); i ++) {
-			if (variables[i] == NULL)
-				continue;
-			VARIABLE *var = variables[i];
-			if (!strcmp(var->identifier, id)) {
-				var->value = nVal;
-				break;
+void add_variable(char *id, float val, int constant) {
+	if (!variable_exists(id)) {
+    	variables[++ variables_count] = create_variable(id, val, constant);
+		return;
+	}
+	for (int i = 0; i < sizeof(variables) / sizeof(VARIABLE); i ++) {
+		if (variables[i] == NULL)
+			continue;
+		VARIABLE *var = variables[i];
+		if (!strcmp(var->identifier, id)) {
+			if (var->constant) {
+				printf("Unable to modify the variable: It is marked as constant.\n");
+				return;
 			}
+			var->constant = constant;
+			var->value = val;
+			break;
 		}
 	}
 }
-
 typedef struct FUNCTION FUNCTION;
 
 struct FUNCTION {
 	char *identifier;
 	char *snippet;
+	int constant;
 };
 
 int functions_count = 0;
 FUNCTION *functions[300];
 
 
-FUNCTION *create_function(char *identifier, char *snippet) {
+FUNCTION *create_function(char *identifier, char *snippet, int constant) {
 	FUNCTION *fun = (FUNCTION*) malloc(sizeof(FUNCTION));
 	fun->snippet = (char*) calloc(100, sizeof(char));
 	fun->identifier = (char*) calloc(100, sizeof(char));
+	fun->constant = constant;
 	strcpy(fun->snippet, snippet);
 	strcpy(fun->identifier, identifier);
 	return fun;
@@ -94,19 +107,23 @@ FUNCTION *create_function(char *identifier, char *snippet) {
 
 int function_exists(char*);
 
-void add_function(char *identifier, char *snippet) {
+void add_function(char *identifier, char *snippet, int constant) {
 	if (!function_exists(identifier)) {
-		functions[++ functions_count] = create_function(identifier, snippet);
+		functions[++ functions_count] = create_function(identifier, snippet, constant);
 		return;
 	}
-	printf("Function '%s' already exists. Modifying ..\n", identifier);
 	for (int i = 0; i < sizeof(functions) / sizeof(FUNCTION); i ++) {
 		if (functions[i] == NULL) {
 			continue;
 		}
 		FUNCTION *fun = functions[i];
 		if (!strcmp(fun->identifier, identifier)) {
+			if (fun->constant) {
+				printf("Unable to modify the function: It is marked as constant.\n");
+				return;
+			}
 			fun->snippet = snippet;
+			fun->constant = constant;
 			return;
 		}
 	}
@@ -186,16 +203,18 @@ void strncatchar(char *str, char c) {
 
 char *identifier() {
 	char *iden = (char*) calloc(100, sizeof(char));
-	while ((current() >= 'a' && current() <= 'z')) {
+	while (IS_LETTER(current())) {
 		strncatchar(iden, current());
 		next();
-		if (!(current() >= 'a' && current() <= 'z')) {
+		if (!IS_LETTER(current())) {
 			last();
 			break;
 		}
 	}
 	return iden;
 }
+
+float parse();
 
 float number() {
 	float number = 0.0;
@@ -207,7 +226,7 @@ float number() {
 		next();
 		return fPass;
 	}
-    if (current() >= 'a' && current() <= 'z') {
+    if (IS_LETTER(current())) {
 		char *iden = identifier();
 		next();
 		if (current() == '[') {
@@ -246,7 +265,7 @@ float number() {
 			char *oldInput = input;
 			pos = 0;
 			input = fun->snippet;
-			float fExpr = expression();
+			float fExpr = parse();
 			pos = oldPos;
 			input = oldInput;
 			return fExpr;
@@ -300,7 +319,7 @@ float number() {
 }
 
 float factor() {
-	if ((current() >= 'a' && current() <= 'z') || (current() >= '0' && current() <= '9') || current() == '$' || current() == '?') {
+	if (IS_LETTER(current()) || IS_DIGIT(current()) || current() == '$' || current() == '?') {
 		return number();
 	}
 	if (current() == '-') {
@@ -334,35 +353,58 @@ float term() {
 float variable() {
 	char *iden = (char*) calloc(100, sizeof(char));
 	float expr;
+	int is_const = 0;
 	if (current() != '@') {
 		printf("Expected expression or '@' on column %d.\n", pos);
 		return 0;
 	}
 	next();
-	if (!(current() >= 'a' && current() <= 'z')) {
+	if (!IS_LETTER(current())) {
 		printf("Expected identifier after '@' on column %d.\n", pos);
 		return 0;
 	}
 	iden = identifier();
 	next();
-	if (current() != '=') {
-		printf("Expected '=' after identifier on column %d.\n", pos);
+	if (current() == '=') {
+		goto _not_const;
+	} else if (current() != '(') {
+		printf("Expected '=' or '(const)' after identifier on column %d.\n", pos);
 		return 0;
 	}
 	next();
-	expr = expression();
-	
-	if (variable_exists(iden)) {
-		printf("Variable '%s' already exists. Modifying ...\n", iden);
-		mod_variable(iden, expr);
+	if (!IS_LETTER(current())) {
+		printf("Expected 'const' after '(' on column %d.\n", pos);
 		return 0;
 	}
-	add_variable(iden, expr);
+	char *const_iden = identifier();
+	next();
+	if (current() != ')') {
+		printf("Expected ')' after 'const' on column %d.\n", pos);
+		return 0;
+	}
+	if (strcmp(const_iden, "const")) {
+		printf("Expected '=' or '(const)' after identifier on column %d.\n", pos);
+		return 0;
+	}
+	is_const = 1;
+
+	next();
+	if (current() != '=') {
+		printf("Expected '=' after '(const)' on column %d.\n", pos);
+		return 0;
+	}
+
+	_not_const:
+	next();
+	expr = expression();
+	
+	add_variable(iden, expr, is_const);
 	return 0;
 }
 
 // #identifier {? - 1}
 float function() {
+	int is_constant = 0;
 	char *iden;
 	char *snippet = (char*) calloc(100, sizeof(char));
 	if (current() != '#') {
@@ -370,12 +412,33 @@ float function() {
 		return 0;
 	}
 	next();
-	if (!(current() >= 'a' && current() <= 'z')) {
+	if (!IS_LETTER(current())) {
 		printf("Expected identifier after '#' on column %d.\n", pos);
 		return 0;
 	}
 	iden = identifier();
 	next();
+	if (current() != '(') {
+		goto fun_not_constant;
+	}
+	next();
+	if (!IS_LETTER(current())) {
+		printf("Expected '(const)' or '{' after identifier on column %d.\n", pos);
+		return 0;
+	}
+	char *const_iden = identifier();
+	next();
+	if (current() != ')') {
+		printf("Expected ')' after 'const' on column %d.\n", pos);
+		return 0;
+	}
+	if (strcmp(const_iden, "const")) {
+		printf("Expected '(const)' or '{' after identifier on column %d.\n", pos);
+		return 0;
+	}
+	is_constant = 1;
+	next();
+	fun_not_constant:
 	if (current() != '{') {
 		printf("Expected '{' after identifier on column %d.\n", pos);
 		return 0;
@@ -389,7 +452,7 @@ float function() {
 		printf("Expected '}' after function body on column %d.\n", pos);
 		return 0;
 	}
-	add_function(iden, snippet);
+	add_function(iden, snippet, is_constant);
 	next();
 }
 
@@ -402,7 +465,7 @@ float parse() {
 		should_output = 0;
 		return function();
 	}
-	if (current() >= '0' && current() <= '9' || current() == '-' || current() >= 'a' && current() <= 'z' || current() == '$' || current() == '?') {
+	if (IS_DIGIT(current()) || IS_LETTER(current()) || current() == '-' || current() == '$' || current() == '?') {
 		return expression();
 	}
 	printf("Unknown expression starting with '%c'.\n", current());
@@ -431,56 +494,89 @@ char *clean(char *input) {
 	return output;
 }
 
+void environment_command() {
+	if (!strcmp(input, ".help")) {
+		printf(".help		- Show this help page\n.clear		- Clear the console\n.last		- Repeat last input\n.exit		- Exit the program\n.quit		- Exit the program\n.lvar		- List variables\n.lfun		- List functions\n");
+		return;
+	} else if (!strcmp(input, ".exit") || !strcmp(input, ".quit")) {
+		should_exit = 1;
+		return;
+	} else if (!strcmp(input, ".clear")) {
+		for (int i = 0; i < 100; i ++) {printf("\n");}
+		printf("Type math expressions here (ex. 1 + 2 * 2)\nUse .help for a list of commands.\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+		return;
+	} else if (!strcmp(input, ".last")) {
+		if (!strcmp(last_input, "")) {
+			printf("There is no last expression.\n");
+			return;
+		}
+		strcpy(input, last_input);
+	} else if (!strcmp(input, ".lvar")) {
+		printf("---------------------------------------------\n");
+		for (int i = 0; i < sizeof(variables) / sizeof(VARIABLE); i ++) {
+			if (variables[i] == NULL) {
+				continue;
+			}
+			VARIABLE *var = variables[i];
+			if (!var->constant) {
+				printf("%s\n .. %f\n", var->identifier, var->value);
+			} else {
+				printf("%s [constant]\n .. %f\n", var->identifier, var->value);
+			}
+		}
+		printf("------------------------------------------\n");
+		return;
+	} else if (!strcmp(input, ".lfun")) {
+		printf("--------------------------------------------\n");
+		for (int i = 0; i < sizeof(functions) / sizeof(FUNCTION); i ++) {
+			if (functions[i] == NULL) {
+				continue;
+			}
+			FUNCTION *fun = functions[i];
+			if (!fun->constant) {
+				printf("%s\n .. %s\n", fun->identifier, fun->snippet);
+			} else {
+				printf("%s [constant]\n .. %s\n", fun->identifier, fun->snippet);
+			}
+		}
+		printf("------------------------------------------\n");
+		return;
+	} else  {
+		printf("Unknown command. Use .help for help.\n");
+		return;
+	}
+}
+
 int main(void) {
-	char *last_input = (char*) calloc(100, sizeof(char));
+	last_input = (char*) calloc(100, sizeof(char));
 	input = (char*) calloc(100, sizeof(char));
-    add_variable("x", 1.5f);
+    add_variable("math_c_version", MATH_C_VERSION, 1);
+	add_function("sin", "sin[?]", 1);
+	add_function("cos", "cos[?]", 1);
+	add_function("tan", "tan[?]", 1);
+	add_function("inv", "inv[?]", 1);
+	add_function("sq", "sq[?]", 1);
+	add_function("sqrt", "sqrt[?]", 1);
 
 	printf("Type math expressions here (ex. 1 + 2 * 2)\nUse .help for a list of commands.\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 
 	while (1) {
-		// fPass = 0.0f;
+		if (should_exit) {
+			return 0;
+		}
 		should_output = 1;
+		pos = 0;
+
 		printf(">>> ");
 		fgets(input, 100, stdin);
 		input = clean(input);
-		pos = 0;
-		if (!strcmp(input, ".help")) {
-			printf(".help		- Show this help page\n.last		- Repeat last input\n.exit		- Exit the program\n.quit		- Exit the program\n.lvar		- List variables\n.lfun		- List functions\n");
-			continue;
-		} else if (!strcmp(input, ".exit") || !strcmp(input, ".quit")) {
-			return 0;
-		} else if (!strcmp(input, ".last")) {
-			if (!strcmp(last_input, "")) {
-				printf("There is no last expression.\n");
-				continue;
-			}
-			strcpy(input, last_input);
-		} else if (!strcmp(input, ".lvar")) {
-			printf("Listing variables ..\n");
-			for (int i = 0; i < sizeof(variables) / sizeof(VARIABLE); i ++) {
-				if (variables[i] == NULL) {
-					continue;
-				}
-				VARIABLE *var = variables[i];
-				printf("%s\n .. %f\n", var->identifier, var->value);
-			}
-			continue;
-		} else if (!strcmp(input, ".lfun")) {
-			printf("Listing functions ..\n");
-			printf("sin\n .. (built in)\ncos\n .. (built in)\ntan\n .. (built in)\ninv\n .. (built in)\nsq\n .. (built in)\nsqrt\n .. (built in)\n");
-			for (int i = 0; i < sizeof(functions) / sizeof(FUNCTION); i ++) {
-				if (functions[i] == NULL) {
-					continue;
-				}
-				FUNCTION *fun = functions[i];
-				printf("%s\n .. %s\n", fun->identifier, fun->snippet);
-			}
-			continue;
-		} else if (input[0] == '.') {
-			printf("Unknown command. Use .help for help.\n");
+
+		if (input[0] == '.') {
+			should_output = 0;
+			environment_command();
 			continue;
 		}
+
 		if (countinstr(input, '(') != countinstr(input, ')')) {
 			printf("Unbalanced open and closed parantheses.\n");
 			continue;	
@@ -493,6 +589,7 @@ int main(void) {
 			printf("Unbalanced open and closed square brackets.\n");
 			continue;	
 		}
+
 		float expr = parse();
 		if (should_output) {
 			printf("= %f\n", expr); 
